ba3698c4413374e78746f0cdbcb428233820ea3b
Jheovanny Campos <jheovanny06@hotmail.com>
2021-10-04 12:02:38 -0300

    git commit -m "Componente Categoria"

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
 
 import { 
     Container,
@@ -46,6 +47,7 @@ export function Register(){
                             isActive={transactionType === 'down'}
                         />
                     </TransactionsTypes>
+                    <CategorySelect title="Categoria" />
                 </Fields>
                 <Button title="Enviar" />
             </Form>
